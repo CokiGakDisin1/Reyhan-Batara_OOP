0631492a830a2355ec7c1c4f3bfa06b3e3fc34bb
CokiGakDisin1 <rbatara06@gmail.com>
2025-11-17 15:29:51 +0700

    CS10_ReyhanBatara

---
+++ frontend_ReyhanBatara/core/src/main/java/com/Reyhan/frontend/states/GameOverState.java
@@ -0,0 +1,16 @@
+package com.Reyhan.frontend.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+
+public class GameOverState implements GameState {
+
+    private final GameStateManager gsm;
+    private final BitmapFont font;
+    private final GlyphLayout layoutGameOver;
+    private final GlyphLayout layoutrestart;
+
+}
---
+++ frontend_ReyhanBatara/core/src/main/java/com/Reyhan/frontend/states/GameState.java
@@ -0,0 +1,9 @@
+package com.Reyhan.frontend.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public interface GameState {
+    public abstract void update(float delta);
+    public abstract void render(SpriteBatch batch);
+    public abstract void dispose();
+}
